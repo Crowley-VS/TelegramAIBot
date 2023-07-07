93bbee7776ffc87350a07ebc126ec2ca0b82613e
Vadim Sevriuk <vadim.sevriuk@gmail.com>
2023-07-07 05:53:31 -0400

    TAB-10 Add functionality for selecting a language

--- bot.py
+++ bot.py
@@ -38,12 +38,44 @@ class TelegramBot:
     def __init__(self, character):
         self.bot = telebot.TeleBot(os.environ.get('TELEGRAM_BOT_KEY'))
         self.character = character
+        self.chats = {}
+
+    def _handle_message(self):
+        self.bot.message_handler(func=lambda msg: True)(self._handle_message_wrapper)
     
+    def _handle_message_wrapper(self, message):
+        self.character.read(message.text)
+        if self.character.name in message.text:
+            self.bot.reply_to(message, self.character.reply())
+
     def start(self):
-        @self.bot.message_handler(func=lambda msg: True)
-        def reply(message):
-            self.character.read(message.text)
-            if self.character.name in message.text:
-                self.bot.reply_to(message, self.character.reply())
+        self._select_language()
+        self._handle_message()
         self.bot.infinity_polling()
+
+    def _select_language(self):
+        self.bot.message_handler(commands=['language'])(self._select_language_wrapper)
+
+    def _select_language_wrapper(self, message):
+        message_text = message.text.strip().strip('/language')
+        try:
+            if not message_text:
+                raise ValueError('Empty message was sent!')
+            language_chance_list = message_text.split()
+            language_chance_dict = {}
+            for i in range(0, len(language_chance_list), 2):
+                try:
+                    chance = int(language_chance_dict[i+1])
+                    if 0 <= chance <= 100:
+                        language_chance_dict[language_chance_list[i]] = int(language_chance_dict[i+1])
+                    else:
+                        raise ValueError
+                except (IndexError, KeyError):
+                    raise ValueError('Message of inappropriate format was sent!')
+                except ValueError:
+                    raise ValueError('{} if an invalid chance. Use a number from 0 to 100.'.format(chance))
+        except ValueError as e:
+            self.bot.reply_to(message, str(e))
+
+
         
