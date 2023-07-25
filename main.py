9a2d2de50fcb686d1a564569e57d5e080a99ba13
Vadim Sevriuk <vadim.sevriuk@gmail.com>
2023-07-25 07:40:21 -0400

    TAB-25 Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import telebot
+from bot import TelegramBot, WebhookManager
+import os
+
+if __name__ == '__main__':
+    webhook_url = os.environ.get('WEBHOOK_URL')  # Get the webhook URL from the environment variables
+
+    if webhook_url is None:
+        raise ValueError("Webhook URL not set. Please provide your Render-provided webhook URL in the environment variables.")
+    
+    bot = TelegramBot()
+    webhook_manager = WebhookManager(bot, webhook_url)
+    webhook_manager.run()
+    
