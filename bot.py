724e7bd03e7e7c728c37822a60679b46914047c7
Vadim Sevriuk <vadim.sevriuk@gmail.com>
2023-09-11 14:16:55 -0400

    Do adjustments for dump_expired_conversations

--- bot.py
+++ bot.py
@@ -618,8 +618,8 @@ class TelegramBot:
 
     def dump_expired_conversations(self):
         while True:
-            # Adjust the sleep time (e.g., 10 minutes)
-            time.sleep(60)
+            # Adjust the sleep time (e.g., 4 minutes)
+            time.sleep(4*60)
             # Get the current timestamp
             current_time = datetime.now()
 
@@ -629,8 +629,8 @@ class TelegramBot:
                 last_access_time = conversation.get_last_access_timestamp()
                 # Calculate the time difference
                 time_difference = current_time - last_access_time
-                # Check if the conversation is expired (last usage > 10 minutes ago)
-                if time_difference.total_seconds() > 30:
+                # Check if the conversation is expired (last usage > 2 minutes ago)
+                if time_difference.total_seconds() > 2*60:
                     print('Logging chat with id: {}'.format(chat_id))
                     self.database_manager.save_conversation(chat_id, conversation)
                     chat_ids_to_delete.add(chat_id)  # Collect chat IDs to delete
